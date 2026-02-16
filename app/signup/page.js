3dc3dc5dc540b548e24b2cff97d2464463709fe5
Ayush Verma <vermaayush9170@gmail.com>
2026-02-16 06:38:35 +0530

    generate page

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React from 'react'
 import SignUp from '@/components/signUp'
 const SignUpPage = () => {
--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 "use client";
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 import React from "react";
 import { useSession, signIn} from "next-auth/react";
